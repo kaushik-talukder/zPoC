4bddea9ca7df854b0ff9120127c3e83ca4dc36ec
Kaushik Talukder <kaushik.talukder@staples.com>
2020-02-24 16:14:09 -0500

    Identifires

---
+++ src/main/java/declarationAndAccessControl/_.java
@@ -0,0 +1,33 @@
+package declarationAndAccessControl;
+
+import static java.lang.System.*;
+
+/*
+
+What is the result?
+A. -A
+B. A.
+C. -A.
+D. _A.
+E. _-A.
+F. Compilation fails
+G. An exception is thrown at runtime 
+
+Answer: B
+
+ */
+
+class _ {
+	public static void main(String[] __A_V_) {
+		// Run time arg
+		__A_V_ = new String[] { "-", "A", "." };
+
+		String $ = "";
+
+		for (int x = 0; ++x < __A_V_.length;)
+			$ += __A_V_[x];
+
+		out.println($);
+
+	}
+}
